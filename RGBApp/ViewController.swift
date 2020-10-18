8afdddc6be2193fc87733f79153ef4e5387e9ab2
Katios <kate.chonkina@gmail.com>
2020-10-18 21:54:38 +0600

    Commit 1

--- RGBApp/ViewController.swift
+++ RGBApp/ViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var windowForColor: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redPercent: UILabel!
+    @IBOutlet var greenPercent: UILabel!
+    @IBOutlet var bluePercent: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        redSlider.value = 0
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.thumbTintColor = .red
+        }
+    func colorPercent() {
+        redPercent.text = ""
+        greenPercent.text = ""
+        bluePercent.text = ""
+        }
 
 
 }
